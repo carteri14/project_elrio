631fd9a12f016e51b72fee38178acb6c476ef355
Alyssa Pingel <alyssa.pingel@my.simpson.edu>
2018-10-30 08:50:46 -0500

    add php files to bootstrap branch

---
+++ public_html/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+  <title>El Rio</title>
+
+    <!-- This says our CSS is compatible with phones, and phones
+         don't need to scale it strangely. -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Main CSS styles for Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- Optional theme add-on -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/lux/bootstrap.min.css">
+    <style>
+       .center {
+       display: block;
+       margin-left: auto;
+       margin-right: auto;
+       
+       }
+    </style>
+</head>
+
+<body>
+    <?php include '../resources/navigation.php'; ?>
+
+
+<header>
+<div class="jumbotron">
+  <h1 class="display-4">El Rio</h1>
+</div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col">
+           <h2 style="text-align:center">World Famous Homemade Margaritas</h2>
+           <p style="font-size:1.2em">El Rio Mexican Restaurant is known for its famous Margaritas and its authentic Mexican food. We are the leading Mexican Restaurant serving in Indianola and in Des Moines. We offer Mexican food, outdoor dining, dine in, take out, and much more. For all your Mexican restaurant needs, visit El Rio today!</p>
+           <img class="center" src="img/el_rio.png" alt="River" />
+           <p style="text-align:center; font-size: 1.2em">No one does Margaritas like El Rio does Margaritas&excl;</p>
+           <br>
+           <br>
+           <br>
+
+           <?php include '../resources/footer.php'; ?>
+        </div>
+    </div>
+</div>
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+  </body>
+</html>
