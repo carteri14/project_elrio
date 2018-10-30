c5c847018704744b379a5aaf396b4e88cd68be7e
Alyssa <alyssa.pingel@my.simpson.edu>
2018-10-30 09:16:41 -0500

    changed nav

--- resources/navigation.php
+++ resources/navigation.php
@@ -1,7 +1,32 @@
 <!-- Navigation Bar -->
-    <h1>My Website</h1>
-    <ul class="nav navbar-nav">
-      <li><a href="example_01.php">Example 1</a></li>
-      <li><a href="example_02.php">Example 2</a></li>
-      <li><a href="example_03.php">Example 3</a></li>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="index.html">El Rio</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!--<div class="d-none d-md-block col-2"> -->
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Home </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="about.html">About<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="drinks.html">Drinks</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="breakfast.html">Breakfast</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="dinner.html">Dinner</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="dessert.html">Dessert</a>
+      </li>
     </ul>
+  </div>
+
+</nav>
